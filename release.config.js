59f2b1d60f821a0486a6a0f0a1a4ac8d2d97174b
kiko <xl1125@outlook.com>
2026-06-18 07:35:51 +0800

    chore: sync all config versions

---
+++ release.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  files: [
+    'package.json',
+    'src-tauri/tauri.conf.json', 
+    'src-tauri/Cargo.toml'
+  ],
+  bumpFiles: [
+    {
+      filename: 'package.json',
+      type: 'json'
+    },
+    {
+      filename: 'src-tauri/tauri.conf.json',
+      type: 'json'
+    },
+    {
+      filename: 'src-tauri/Cargo.toml',
+      type: 'toml'
+    }
+  ],
+  commit: true,
+  tag: true,
+  push: true,
+  skip: {
+    tag: false
+  },
+  preset: 'conventionalcommits',
+  types: [
+    { type: 'feat', section: 'Features' },
+    { type: 'fix', section: 'Bug Fixes' },
+    { type: 'docs', section: 'Documentation' },
+    { type: 'style', section: 'Styles' },
+    { type: 'refactor', section: 'Code Refactoring' },
+    { type: 'perf', section: 'Performance' },
+    { type: 'test', section: 'Tests' },
+    { type: 'build', section: 'Builds' },
+    { type: 'ci', section: 'Continuous Integrations' },
+    { type: 'chore', section: 'Chores' },
+    { type: 'revert', section: 'Reverts' }
+  ]
+}
